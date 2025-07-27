5135c471e7b53d65ea0ae1052e8c8340971ad4f2
Bruno Franco <brunoantunesfranco@gmail.com>
2025-07-27 23:02:50 +0100

    fix pool

--- incs/core/data_structures/pool.hpp
+++ incs/core/data_structures/pool.hpp
@@ -6,7 +6,7 @@
 /*   By: bfranco <bfranco@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/25 17:39:42 by bfranco       #+#    #+#                 */
-/*   Updated: 2024/10/30 23:36:08 by bfranco       ########   odam.nl         */
+/*   Updated: 2025/07/27 22:59:20 by bfranco       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,31 @@ class Pool
 		class Object
 		{
 			private:
-				Pool*		_pool;
-				TType*		_ptr;
-				size_t		_index;
+				Pool*					_pool;
+				size_t					_index;
+				std::shared_ptr<TType>	_ptr;
 
 			public:
-				Object(Pool* pool, TType* ptr, size_t index) : _pool(pool), _ptr(ptr), _index(index) {}
-				~Object() { if (_ptr) _pool->release(_index); }
+				Object(Pool* pool, TType* ptr, size_t index) : _pool(pool), _index(index) {
+					_ptr = std::shared_ptr<TType>(ptr, [pool, index](TType* p) {
+    				    p->~TType();  pool->release(index);
+    				});
+				}
+				~Object() = default;
+				
+				Object(const Object &other) { *this = other; }
+				Object	&operator=(const Object &other) {
+					if (this != &other) {
+						auto tmp = other._pool->acquire(other._ptr);
+						_pool = tmp._pool;
+						_index = tmp._index;
+						_ptr = std::move(tmp._ptr);
+					}
+					return *this;
+				}
 
-				Object(const Object &other) = delete;
-				Object	&operator=(const Object &other) = delete;
-				Object(Object &other) = delete;
-				Object	&operator=(Object &other) = delete;
-
-				TType*	operator->() { return _ptr; }
+				TType* operator->() { return _ptr.get(); };
+				TType& operator*() { return *_ptr; };
 		};
 		
 		Pool(size_t poolSize = 5);
@@ -54,6 +65,9 @@ class Pool
 		Object	acquire(TArgs&&... p_args);
 		void	resize(const size_t numberOfObjectStored);
 		void	release(size_t index);
+		size_t	size() const;
+		void	add(Object& obj);
+		
 };
 
 template<typename TType>
@@ -74,18 +88,7 @@ void Pool<TType>::resize(const size_t numberOfObjectStored)
 }
 
 template<typename TType>
-void Pool<TType>::release(size_t index)
-{
-	try
-	{
-		_pool.at(index).~TType();
-		_available.push(index);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-}
+void Pool<TType>::release(size_t index) { _available.push(index); }
 
 template<typename TType>
 template<typename ... TArgs>
@@ -97,8 +100,13 @@ typename Pool<TType>::Object Pool<TType>::acquire(TArgs&&... p_args)
 	size_t	index = _available.top();
 	_available.pop();
 
-	new (&(_pool.at(index))) TType(std::forward<decltype(TArgs(p_args))>(p_args)...);
-	return Object(this, &(_pool.at(index)), index);
+	void* mem = &_pool[index];
+	TType* objPtr = new (mem) TType(std::forward<decltype(TArgs(p_args))>(p_args)...);
+	return Object(this, objPtr, index);
 }
+
+template<typename TType>
+size_t Pool<TType>::size() const { return _poolSize; }
+
 
 #endif
